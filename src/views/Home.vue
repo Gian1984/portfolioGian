12af7b929b2ce6d1098f4eae75c1b242309dc2c2
Gianluca Tiengo <gl.tiengo@gmail.com>
2026-03-04 14:43:06 +0100

    fix implementation of Turnstile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -280,15 +280,28 @@ export default {
     },
 
     created() {
-        window.onTurnstileSuccess = (token) => {
-            this.turnstileToken = token;
-        };
-        window.onTurnstileExpired = () => {
-            this.turnstileToken = '';
+        window.onloadTurnstileCallback = () => {
+            this.renderTurnstile();
         };
     },
 
+    mounted() {
+        this.$nextTick(() => {
+            if (window.turnstile) {
+                this.renderTurnstile();
+            }
+        });
+    },
+
     methods: {
+        renderTurnstile() {
+            window.turnstile.render('#turnstile-widget', {
+                sitekey: '0x4AAAAAACmDmw9moS3XzJjw',
+                callback: (token) => { this.turnstileToken = token; },
+                'expired-callback': () => { this.turnstileToken = ''; },
+            });
+        },
+
         sendEmail() {
             try {
                 emailjs.sendForm('service_58mft2p', 'template_dhvckrb', this.$refs.form, 'AmnMprCMW3riak6qR', {
@@ -685,12 +698,7 @@ export default {
               </div>
               <div class="relative mb-4">
                 <!-- Replace YOUR_TURNSTILE_SITE_KEY with your key from dash.cloudflare.com/turnstile -->
-                <div
-                  class="cf-turnstile"
-                  data-sitekey="0x4AAAAAACmDmw9moS3XzJjw"
-                  data-callback="onTurnstileSuccess"
-                  data-expired-callback="onTurnstileExpired"
-                ></div>
+                <div id="turnstile-widget"></div>
               </div>
               <button v-if="turnstileToken" type="submit" value="Send" class="mt-4 w-24 flex bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                 <span class="flex">
